df746cc21a44d8bc95335348541a54232e8f9db7
Luciano Muniz <luciano.mo@gmail.com>
2022-10-14 01:06:24 -0300

    added coffee shop list and coffee shop card components

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import Banner from '../components/banner';
+import Card from '../components/card';
+
+import { fetchCoffeeStores } from '../lib/coffee-stores';
+
+// import coffeeStoresData from '../data/coffee-store.json';
+
+import styles from '../styles/home.module.css';
+
+export async function getStaticProps(context) {
+  const coffeeStores = await fetchCoffeeStores();
+
+  return {
+    props: {
+      coffeeStores,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const handleOnBannerBtnClick = () => {
+    console.log('Hi banner button');
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Coffee Connoisseur</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <Banner
+          buttonText='View store nearby'
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <div className={styles.heroImage}>
+          <Image src='/static/hero-image.png' width={700} height={400} />
+        </div>
+        {props.coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Toronto stores</h2>
+
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
+      </main>
+    </div>
+  );
+}
